dae072e151530bf281698a12a577755b1c5749a3
Göran Lilja <goran.lilja@visma.com>
2017-12-04 12:23:02 +0100

    Printing pattern.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,37 @@
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight)
+  background(51)
+  stroke(200)
+  noFill()
 }
 
+let cellSize = 20
+let x = 0
+let y = 0
+
 function draw() {
-  // put drawing code here
+  if (x >= width) {
+    x = 0
+    y += cellSize
+  }
+  if (y >= height) {
+    x = 0
+    y = 0
+    background(51)
+  }
+
+  if (floor(random(0, 2)) == 1) { // \
+    line(x, y, x+cellSize, y+cellSize)
+  } else { // /
+    line(x+cellSize, y, x, y+cellSize)
+  }
+
+  x += cellSize
+}
+
+function restart(newCellSize = 10) {
+  cellSize = newCellSize
+  background(51)
+  x = 0
+  y = 0
 }
